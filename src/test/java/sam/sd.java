a65b8b7d1a467375afd9d5ab39322c7d163d0d66
Dany1407 <vigneshsuriya7@gmail.com>
2021-02-25 00:37:38 +0530

    new code

--- src/test/java/sam/sd.java
+++ src/test/java/sam/sd.java
@@ -3,7 +3,8 @@ package sam;
 public class sd {
 	public static void main(String[] args) {
 		System.out.println("kjkjkjkj");
-		System.out.println("kjjkjkk");
+		System.out.println("klkl333");
+		System.out.println("kjkjknknkn");
 	}
 
 }
